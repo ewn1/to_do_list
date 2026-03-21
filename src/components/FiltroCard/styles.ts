ac27223bf63912805226ec435d81065591c2da21
Edwin <ewn.francisco@gmail.com>
2026-03-21 17:47:33 -0300

    criando o filtro por prioridade e adicionando o contador

--- src/components/FiltroCard/styles.ts
+++ src/components/FiltroCard/styles.ts
@@ -1,15 +1,16 @@
 import styled from 'styled-components'
 
-import { Props } from '.'
+type Props = {
+  ativo: boolean
+}
 
-type PropsSemLegendaEContador = Omit<Props, 'contador' | 'legenda'> //Utilizamos "Omit" para omitir 2 das Props que declaramos no componente FiltroCard porque ela tbm estava sendo usada em styles
-
-export const Card = styled.div<PropsSemLegendaEContador>`
+export const Card = styled.div<Props>`
   padding: 8px;
   border: 1px solid ${(props) => (props.ativo ? '#1e90ff' : '#a1a1a1')};
   background-color: ${(props) => (props.ativo ? '#fff' : '#fcfcfc')};
   color: ${(props) => (props.ativo ? '#1e90ff' : '#5e5e5e')};
   border-radius: 8px;
+  cursor: pointer;
 `
 
 export const Contador = styled.span`
